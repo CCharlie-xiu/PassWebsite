347dbccc1c20b0a093e228cb24f9b6becfa37599
wuchanghua <1453346832@qq.com>
2022-10-16 17:47:38 +0800

    2.3.0

---
+++ json.js
@@ -0,0 +1,604 @@
+
+			var data = [{
+					id: 1,
+					gather: [{
+						id: 11,
+						url: "https://www.mgtv.com/b/291976/3094630.html"
+					},{
+						id: 12,
+						url: "https://www.mgtv.com/b/291976/3108594.html"
+					},{
+						id: 13,
+						url: "https://www.mgtv.com/b/291976/3117857.html"
+					},{
+						id: 14,
+						url: "https://www.mgtv.com/b/291976/3126857.html"
+					},{
+						id: 15,
+						url: "https://www.mgtv.com/b/291976/3136900.html"
+					},{
+						id: 16,
+						url: "https://www.mgtv.com/b/291976/3145262.html"
+					},{
+						id: 17,
+						url: "https://www.mgtv.com/b/291976/3165810.html"
+					},{
+						id: 18,
+						url: "https://www.mgtv.com/b/291976/3176355.html"
+					},{
+						id: 19,
+						url: "https://www.mgtv.com/b/291976/3195870.html"
+					},{
+						id: 110,
+						url: "https://www.mgtv.com/b/291976/3247820.html"
+					},{
+						id: 111,
+						url: "https://www.mgtv.com/b/291976/3272630.html"
+					},{
+						id: 112,
+						url: "https://www.mgtv.com/b/291976/3283295.html"
+					},{
+						id: 113,
+						url: "https://www.mgtv.com/b/291976/3285994.html"
+					}]
+				},{
+					id: 2,
+					gather: [{
+						id: 21,
+						url: "https://www.mgtv.com/b/309556/3789850.html"
+					},{
+						id: 22,
+						url: "https://www.mgtv.com/b/309556/3802828.html"
+					},{
+						id: 23,
+						url: "https://www.mgtv.com/b/309556/3808409.html"
+					},{
+						id: 24,
+						url: "https://www.mgtv.com/b/309556/3825108.html"
+					},{
+						id: 25,
+						url: "https://www.mgtv.com/b/309556/3838070.html"
+					},{
+						id: 26,
+						url: "https://www.mgtv.com/b/309556/3849471.html"
+					},{
+						id: 27,
+						url: "https://www.mgtv.com/b/309556/3858761.html"
+					},{
+						id: 28,
+						url: "https://www.mgtv.com/b/309556/3867727.html"
+					},{
+						id: 29,
+						url: "https://www.mgtv.com/b/309556/3876344.html"
+					},{
+						id: 210,
+						url: "https://www.mgtv.com/b/309556/3884336.html"
+					},{
+						id: 211,
+						url: "https://www.mgtv.com/b/309556/3891649.html"
+					},{
+						id: 212,
+						url: "https://www.mgtv.com/b/309556/3899508.html"
+					},{
+						id: 212,
+						url: "https://www.mgtv.com/b/309556/3899510.html"
+					}]
+				},{
+					id: 3,
+					gather: [{
+						id: 31,
+						url: "https://www.mgtv.com/b/317663/4157493.html"
+					},{
+						id: 32,
+						url: "https://www.mgtv.com/b/317663/4157494.html"
+					},{
+						id: 33,
+						url: "https://www.mgtv.com/b/317663/4164958.html"
+					},{
+						id: 34,
+						url: "https://www.mgtv.com/b/317663/4164959.html"
+					},{
+						id: 35,
+						url: "https://www.mgtv.com/b/317663/4173475.html"
+					},{
+						id: 36,
+						url: "https://www.mgtv.com/b/317663/4173476.html"
+					},{
+						id: 37,
+						url: "https://www.mgtv.com/b/317663/4182554.html"
+					},{
+						id: 38,
+						url: "https://www.mgtv.com/b/317663/4182555.html"
+					},{
+						id: 39,
+						url: "https://www.mgtv.com/b/317663/4192154.html"
+					},{
+						id: 310,
+						url: "https://www.mgtv.com/b/317663/4192155.html"
+					},{
+						id: 311,
+						url: "https://www.mgtv.com/b/317663/4201636.html"
+					},{
+						id: 312,
+						url: "https://www.mgtv.com/b/317663/4201664.html"
+					},{
+						id: 313,
+						url: "https://www.mgtv.com/b/317663/4210521.html"
+					},{
+						id: 314,
+						url: "https://www.mgtv.com/b/317663/4210522.html"
+					},{
+						id: 315,
+						url: "https://www.mgtv.com/b/317663/4219884.html"
+					},{
+						id: 316,
+						url: "https://www.mgtv.com/b/317663/4219889.html"
+					},{
+						id: 317,
+						url: "https://www.mgtv.com/b/317663/4229032.html"
+					},{
+						id: 318,
+						url: "https://www.mgtv.com/b/317663/4229072.html"
+					},{
+						id: 319,
+						url: "https://www.mgtv.com/b/317663/4235469.html"
+					},{
+						id: 320,
+						url: "https://www.mgtv.com/b/317663/4235470.html"
+					},{
+						id: 321,
+						url: "https://www.mgtv.com/b/317663/4243924.html"
+					},{
+						id: 322,
+						url: "https://www.mgtv.com/b/317663/4243925.html"
+					},{
+						id: 323,
+						url: "https://www.mgtv.com/b/317663/4251993.html"
+					},{
+						id: 324,
+						url: "https://www.mgtv.com/b/317663/4251994.html"
+					},{
+						id: 325,
+						url: "https://www.mgtv.com/b/317663/4260103.html"
+					},]
+				},{
+					id: 4,
+					gather: [{
+						id: 41,
+						url: "https://www.mgtv.com/b/325963/4686416.html"
+					},{
+						id: 42,
+						url: "https://www.mgtv.com/b/325963/4686423.html"
+					},{
+						id: 43,
+						url: "https://www.mgtv.com/b/325963/4709240.html"
+					},{
+						id: 44,
+						url: "https://www.mgtv.com/b/325963/4709242.html"
+					},{
+						id: 45,
+						url: "https://www.mgtv.com/b/325963/4733222.html"
+					},{
+						id: 46,
+						url: "https://www.mgtv.com/b/325963/4733223.html"
+					},{
+						id: 47,
+						url: "https://www.mgtv.com/b/325963/4753810.html"
+					},{
+						id: 48,
+						url: "https://www.mgtv.com/b/325963/4753826.html"
+					},{
+						id: 49,
+						url: "https://www.mgtv.com/b/325963/4774036.html"
+					},{
+						id: 410,
+						url: "https://www.mgtv.com/b/325963/4774037.html"
+					},{
+						id: 411,
+						url: "https://www.mgtv.com/b/325963/4794024.html"
+					},{
+						id: 412,
+						url: "https://www.mgtv.com/b/325963/4794025.html"
+					},{
+						id: 413,
+						url: "https://www.mgtv.com/b/325963/4816038.html"
+					},{
+						id: 414,
+						url: "https://www.mgtv.com/b/325963/4816039.html"
+					},{
+						id: 415,
+						url: "https://www.mgtv.com/b/325963/4841018.html"
+					},{
+						id: 416,
+						url: "https://www.mgtv.com/b/325963/4841019.html"
+					},{
+						id: 417,
+						url: "https://www.mgtv.com/b/325963/4878254.html"
+					},{
+						id: 418,
+						url: "https://www.mgtv.com/b/325963/4878256.html"
+					},{
+						id: 419,
+						url: "https://www.mgtv.com/b/325963/4922763.html"
+					},{
+						id: 420,
+						url: "https://www.mgtv.com/b/325963/4925364.html"
+					},{
+						id: 421,
+						url: "https://www.mgtv.com/b/325963/4972386.html"
+					},{
+						id: 422,
+						url: "https://www.mgtv.com/b/325963/4971683.html"
+					},{
+						id: 423,
+						url: "https://www.mgtv.com/b/325963/5012431.html"
+					},{
+						id: 424,
+						url: "https://www.mgtv.com/b/325963/5009236.html"
+					}]
+				},{
+					id: 5,
+					gather: [{
+						id: 51,
+						url: "https://www.mgtv.com/b/331915/6868563.html"
+					},{
+						id: 52,
+						url: "https://www.mgtv.com/b/331915/6948293.html"
+					},{
+						id: 53,
+						url: "https://www.mgtv.com/b/331915/6948413.html"
+					},{
+						id: 54,
+						url: "https://www.mgtv.com/b/331915/7025523.html"
+					},{
+						id: 55,
+						url: "https://www.mgtv.com/b/331915/7025539.html"
+					},{
+						id: 56,
+						url: "https://www.mgtv.com/b/331915/7106114.html"
+					},{
+						id: 57,
+						url: "https://www.mgtv.com/b/331915/7106138.html"
+					},{
+						id: 58,
+						url: "https://www.mgtv.com/b/331915/7192402.html"
+					},{
+						id: 59,
+						url: "https://www.mgtv.com/b/331915/7192419.html"
+					},{
+						id: 510,
+						url: "https://www.mgtv.com/b/331915/7260434.html"
+					},{
+						id: 511,
+						url: "https://www.mgtv.com/b/331915/7260496.html"
+					},{
+						id: 512,
+						url: "https://www.mgtv.com/b/331915/7314811.html"
+					},{
+						id: 513,
+						url: "https://www.mgtv.com/b/331915/7314812.html"
+					},{
+						id: 514,
+						url: "https://www.mgtv.com/b/331915/7354380.html"
+					},{
+						id: 515,
+						url: "https://www.mgtv.com/b/331915/7354370.html"
+					},{
+						id: 516,
+						url: "https://www.mgtv.com/b/331915/7386402.html"
+					},{
+						id: 517,
+						url: "https://www.mgtv.com/b/331915/7386430.html"
+					},{
+						id: 518,
+						url: "https://www.mgtv.com/b/331915/7413034.html"
+					},{
+						id: 519,
+						url: "https://www.mgtv.com/b/331915/7413083.html"
+					},{
+						id: 520,
+						url: "https://www.mgtv.com/b/331915/7442464.html"
+					},{
+						id: 521,
+						url: "https://www.mgtv.com/b/331915/7442468.html"
+					},{
+						id: 522,
+						url: "https://www.mgtv.com/b/331915/7464383.html"
+					},{
+						id: 523,
+						url: "https://www.mgtv.com/b/331915/7464470.html"
+					},{
+						id: 524,
+						url: "https://www.mgtv.com/b/331915/7486493.html"
+					},{
+						id: 525,
+						url: "https://www.mgtv.com/b/331915/7486494.html"
+					}]
+				},{
+					id: 6,
+					gather: [{
+						id: 61,
+						url: "https://www.mgtv.com/b/350860/10515931.html"
+					},{
+						id: 62,
+						url: "https://www.mgtv.com/b/350860/10515932.html"
+					},{
+						id: 63,
+						url: "https://www.mgtv.com/b/350860/10703718.html"
+					},{
+						id: 64,
+						url: "https://www.mgtv.com/b/350860/10704343.html"
+					},{
+						id: 65,
+						url: "https://www.mgtv.com/b/350860/10718686.html"
+					},{
+						id: 66,
+						url: "https://www.mgtv.com/b/350860/10760608.html"
+					},{
+						id: 67,
+						url: "https://www.mgtv.com/b/350860/10770579.html"
+					},{
+						id: 68,
+						url: "https://www.mgtv.com/b/350860/10777574.html"
+					},{
+						id: 69,
+						url: "https://www.mgtv.com/b/350860/10808465.html"
+					},{
+						id: 610,
+						url: "https://www.mgtv.com/b/350860/10817312.html"
+					},{
+						id: 611,
+						url: "https://www.mgtv.com/b/350860/10822193.html"
+					},{
+						id: 612,
+						url: "https://www.mgtv.com/b/350860/10869272.html"
+					},{
+						id: 613,
+						url: "https://www.mgtv.com/b/350860/10879836.html"
+					},{
+						id: 614,
+						url: "https://www.mgtv.com/b/350860/10890396.html"
+					},{
+						id: 615,
+						url: "https://www.mgtv.com/b/350860/10931316.html"
+					},{
+						id: 616,
+						url: "https://www.mgtv.com/b/350860/10940724.html"
+					},{
+						id: 617,
+						url: "https://www.mgtv.com/b/350860/10946500.html"
+					},{
+						id: 618,
+						url: "https://www.mgtv.com/b/350860/10981139.html"
+					},{
+						id: 619,
+						url: "https://www.mgtv.com/b/350860/10989471.html"
+					},{
+						id: 620,
+						url: "https://www.mgtv.com/b/350860/10994224.html"
+					},{
+						id: 621,
+						url: "https://www.mgtv.com/b/350860/11039100.html"
+					},{
+						id: 622,
+						url: "https://www.mgtv.com/b/350860/11050197.html"
+					},{
+						id: 623,
+						url: "https://www.mgtv.com/b/350860/11059815.html"
+					},{
+						id: 624,
+						url: "https://www.mgtv.com/b/350860/11090917.html"
+					},{
+						id: 625,
+						url: "https://www.mgtv.com/b/350860/11088817.html"
+					},{
+						id: 626,
+						url: "https://www.mgtv.com/b/350860/11091652.html"
+					},{
+						id: 627,
+						url: "https://www.mgtv.com/b/350860/11106376.html"
+					},{
+						id: 628,
+						url: "https://www.mgtv.com/b/350860/11106703.html"
+					},{
+						id: 629,
+						url: "https://www.mgtv.com/b/350860/11106750.html"
+					},{
+						id: 630,
+						url: "https://www.mgtv.com/b/350860/11171986.html"
+					},{
+						id: 631,
+						url: "https://www.mgtv.com/b/350860/11183169.html"
+					},{
+						id: 632,
+						url: "https://www.mgtv.com/b/350860/11194234.html"
+					},{
+						id: 633,
+						url: "https://www.mgtv.com/b/350860/11234680.html"
+					},{
+						id: 634,
+						url: "https://www.mgtv.com/b/350860/11243178.html"
+					},{
+						id: 635,
+						url: "https://www.mgtv.com/b/350860/11251991.html"
+					},{
+						id: 636,
+						url: "https://www.mgtv.com/b/350860/11301271.html"
+					},{
+						id: 637,
+						url: "https://www.mgtv.com/b/350860/11311446.html"
+					},{
+						id: 638,
+						url: "https://www.mgtv.com/b/350860/11321229.html"
+					},{
+						id: 639,
+						url: "https://www.mgtv.com/b/350860/11368171.html"
+					},]
+				},{
+					id: 7,
+					gather: [{
+						id: 71,
+						url: "https://www.mgtv.com/b/411016/15381819.html"
+					},{
+						id: 72,
+						url: "https://www.mgtv.com/b/411016/15381825.html"
+					},{
+						id: 73,
+						url: "https://www.mgtv.com/b/411016/15454120.html"
+					},{
+						id: 74,
+						url: "https://www.mgtv.com/b/411016/15456974.html"
+					},{
+						id: 75,
+						url: "https://www.mgtv.com/b/411016/15456975.html"
+					},{
+						id: 76,
+						url: "https://www.mgtv.com/b/411016/15471190.html"
+					},{
+						id: 77,
+						url: "https://www.mgtv.com/b/411016/15527373.html"
+					},{
+						id: 78,
+						url: "https://www.mgtv.com/b/411016/15532900.html"
+					},{
+						id: 79,
+						url: "https://www.mgtv.com/b/411016/15532903.html"
+					},{
+						id: 710,
+						url: "https://www.mgtv.com/b/411016/15550753.html"
+					},{
+						id: 711,
+						url: "https://www.mgtv.com/b/411016/15570652.html"
+					},{
+						id: 712,
+						url: "https://www.mgtv.com/b/411016/15579695.html"
+					},{
+						id: 713,
+						url: "https://www.mgtv.com/b/411016/15579696.html"
+					},{
+						id: 714,
+						url: "https://www.mgtv.com/b/411016/15603818.html"
+					},{
+						id: 715,
+						url: "https://www.mgtv.com/b/411016/15625007.html"
+					},{
+						id: 716,
+						url: "https://www.mgtv.com/b/411016/15634205.html"
+					},{
+						id: 717,
+						url: "https://www.mgtv.com/b/411016/15634206.html"
+					},{
+						id: 718,
+						url: "https://www.mgtv.com/b/411016/15648455.html"
+					},{
+						id: 719,
+						url: "https://www.mgtv.com/b/411016/15671504.html"
+					},{
+						id: 720,
+						url: "https://www.mgtv.com/b/411016/15677141.html"
+					},{
+						id: 721,
+						url: "https://www.mgtv.com/b/411016/15682080.html"
+					},{
+						id: 722,
+						url: "https://www.mgtv.com/b/411016/15682081.html"
+					},{
+						id: 723,
+						url: "https://www.mgtv.com/b/411016/15699053.html"
+					},{
+						id: 724,
+						url: "https://www.mgtv.com/b/411016/15715733.html"
+					},{
+						id: 725,
+						url: "https://www.mgtv.com/b/411016/15722554.html"
+					},{
+						id: 726,
+						url: "https://www.mgtv.com/b/411016/15728848.html"
+					},{
+						id: 727,
+						url: "https://www.mgtv.com/b/411016/15808854.html"
+					},{
+						id: 728,
+						url: "https://www.mgtv.com/b/411016/15862870.html"
+					},{
+						id: 729,
+						url: "https://www.mgtv.com/b/411016/15862923.html"
+					},{
+						id: 730,
+						url: "https://www.mgtv.com/b/411016/15873099.html"
+					},{
+						id: 731,
+						url: "https://www.mgtv.com/b/411016/15873115.html"
+					},{
+						id: 732,
+						url: "https://www.mgtv.com/b/411016/15873135.html"
+					},{
+						id: 733,
+						url: "https://www.mgtv.com/b/411016/15889595.html"
+					},{
+						id: 734,
+						url: "https://www.mgtv.com/b/411016/15957052.html"
+					},{
+						id: 735,
+						url: "https://www.mgtv.com/b/411016/15967433.html"
+					},{
+						id: 736,
+						url: "https://www.mgtv.com/b/411016/15967462.html"
+					},{
+						id: 737,
+						url: "https://www.mgtv.com/b/411016/15967474.html"
+					},{
+						id: 738,
+						url: "https://www.mgtv.com/b/411016/15991849.html"
+					},{
+						id: 739,
+						url: "https://www.mgtv.com/b/411016/16017942.html"
+					},{
+						id: 740,
+						url: "https://www.mgtv.com/b/411016/16019882.html"
+					},{
+						id: 741,
+						url: "https://www.mgtv.com/b/411016/16019883.html"
+					},{
+						id: 742,
+						url: "https://www.mgtv.com/b/411016/16019884.html"
+					},{
+						id: 743,
+						url: "https://www.mgtv.com/b/411016/16055508.html"
+					},{
+						id: 744,
+						url: "https://www.mgtv.com/b/411016/16086138.html"
+					},{
+						id: 745,
+						url: "https://www.mgtv.com/b/411016/16090697.html"
+					},{
+						id: 746,
+						url: "https://www.mgtv.com/b/411016/16090717.html"
+					},{
+						id: 747,
+						url: "https://www.mgtv.com/b/411016/16090725.html"
+					},{
+						id: 748,
+						url: "https://www.mgtv.com/b/411016/16128022.html"
+					},{
+						id: 749,
+						url: "https://www.mgtv.com/b/411016/16134782.html"
+					},{
+						id: 750,
+						url: "https://www.mgtv.com/b/411016/16147869.html"
+					},{
+						id: 751,
+						url: "https://www.mgtv.com/b/411016/16183401.html"
+					},{
+						id: 752,
+						url: "https://www.mgtv.com/b/411016/16183402.html"
+					},{
+						id: 753,
+						url: "https://www.mgtv.com/b/411016/16189648.html"
+					},{
+						id: 754,
+						url: "https://www.mgtv.com/b/411016/16211584.html"
+					}]
+				}]
+				
+			console.log(data[0].gather[0].url)
